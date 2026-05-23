1b92e295daf3ae5d8bff3907def5cca6ecd15846
karbrabamdipankar-art <karbrabamdipankar@gmail.com>
2026-05-23 21:04:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@
 window.TB_CONFIG = {
 
   // Your Google Apps Script Web App URL
-  scriptUrl: 'https://script.google.com/macros/s/AKfycbw2VK6K8TWQRJfUVLD4DBcH4x7fDXLRZyoqFKV76PwcN6GENmDjsGEWsaQ8o6_j9V7WYA/exec',
+  scriptUrl: 'https://script.google.com/macros/s/AKfycbzuvgSzzNw8VDZRv_DRhfdu5iyqK-cTbKEuqeHLfp6HwuyVIT3XAsDGhVuyNYPxSgWbyw/exec',
 
   appName:         'TalentBridge',
   version:         '2.0.0',
